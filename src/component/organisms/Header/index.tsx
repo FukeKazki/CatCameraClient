cc6f483f61026f9077a3ea459047ecc1d4285bd4
FukeKazki <kazkichi0906@gmail.com>
2020-11-03 00:31:21 +0900

    Add: Headerの作成(#24)

---
+++ src/component/organisms/Header/index.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react'
+import { Header as NBHeader, Button, Left } from 'native-base'
+import { MaterialIcons } from '@expo/vector-icons'
+
+import styles from './styles'
+import { activeColor } from '~/utils/colors'
+
+type Props = {
+	openDrawer: () => void
+}
+
+const Header: FC<Props> = ({ openDrawer }) => (
+	<NBHeader style={styles.container}>
+		<Left>
+			<Button transparent onPress={openDrawer}>
+				<MaterialIcons name='menu' size={25} color={activeColor} />
+			</Button>
+		</Left>
+	</NBHeader>
+)
+
+export default Header
