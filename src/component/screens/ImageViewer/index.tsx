f7e97e10cfaa20cf927f461ab78ab7ebc14709d3
FukeKazki <kazkichi0906@gmail.com>
2020-12-11 03:03:27 +0900

    Fix: スタイルの調整 (#35)

--- src/component/screens/ImageViewer/index.tsx
+++ src/component/screens/ImageViewer/index.tsx
@@ -1,10 +1,11 @@
-import React, { FC, ReactNode, useState } from 'react'
-import { View, Image, Dimensions, Text } from 'react-native'
-import { Icon, Button } from 'native-base'
+import React, { FC } from 'react'
+import { View, Image } from 'react-native'
 import ImageZoom from 'react-native-image-pan-zoom'
 import { NavigationStackProp } from 'react-navigation-stack'
+import { windowWidth, windowHeight } from '~/lib/style/styles'
 
 import styles from './styles'
+import ImageViewerHeader from '~/component/screens/ImageViewer/Header'
 
 type NavigateProps = {
 	url: string
@@ -15,44 +16,32 @@ type Props = {
 }
 
 const ImageViewer: FC<Props> = ({ navigation }) => {
-	const [url, setUrl] = useState(navigation.state.params?.url)
-
-	const [windowSize, setWindowSize] = useState({
-		width: Dimensions.get('window').width,
-		height: Dimensions.get('window').height
-	})
+	const url = navigation.state.params?.url
 
 	const handleClose = () => {
 		navigation.goBack()
 	}
 
-  return (
-    <>
-      <View
-        style={styles.container}
-      >
-        <ImageZoom
-          cropWidth={windowSize.width}
-          cropHeight={windowSize.height}
-          imageWidth={windowSize.width}
-          imageHeight={windowSize.height}
-        >
-          <Image
-            style={styles.image}
-            source={{
-              uri: `${url}`
-            }}
-            resizeMode='contain'
-          />
-        </ImageZoom>
-      </View>
-      <Icon
-        name='close'
-        style={styles.closeIcon}
-        onPress={handleClose}
-      />
-    </>
-  )
+	return (
+		<View style={styles.container}>
+			<ImageViewerHeader handleLeftIconClick={handleClose} />
+			<ImageZoom
+				cropWidth={windowWidth}
+				cropHeight={windowHeight}
+				imageWidth={windowWidth}
+				imageHeight={windowHeight}
+				style={styles.imageContainer}
+			>
+				<Image
+					style={styles.image}
+					source={{
+						uri: `${url}`
+					}}
+					resizeMode='contain'
+				/>
+			</ImageZoom>
+		</View>
+	)
 }
 
 export default ImageViewer
--- src/component/screens/ImageViewer/styles.ts
+++ src/component/screens/ImageViewer/styles.ts
@@ -1,28 +1,21 @@
 import { StyleSheet } from 'react-native'
 
 import { windowWidth, windowHeight } from '~/lib/style/styles'
-import { componentBaseColor, inactiveColor } from '~/utils/colors'
+import { componentBaseColor } from '~/utils/colors'
 
 export default StyleSheet.create({
-  container: {
-	  ...StyleSheet.absoluteFillObject,
-	  backgroundColor: componentBaseColor,
-	  overflow: 'hidden',
-	  height: windowHeight
-  },
-  contentContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
+	container: {
+		backgroundColor: componentBaseColor,
+		flex: 1
+	},
+	imageContainer: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	image: {
 		width: windowWidth,
 		height: windowHeight
-  },
-  closeIcon: {
-    position: 'absolute',
-    top: windowWidth / 30,
-    left: windowWidth / 30,
-    fontSize: windowWidth / 10,
-	  color: inactiveColor
-  }
+	}
 })
