0cab61cf49f5fa2f1bc24905837eb0b6c0693c9b
FukeKazki <kazkichi0906@gmail.com>
2020-12-31 18:09:14 +0900

    Update: 写真アクセスリクエストの追加 (#14)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import AppNavigator from './src/lib/navigation/navigation'
 import { UploadProvider } from '~/lib/context/upload'
 import { DailyImagesProvider } from '~/lib/context/dailyImages'
+import { requestPermissionsAsync, getPermissionsAsync } from 'expo-media-library'
 
 export default function App() {
+	useEffect(() => {
+		getPermissionsAsync()
+			.then(res => {
+				if (!res.granted) {
+					requestPermissionsAsync()
+						.catch(e => {
+							console.error(e)
+						})
+				}
+			})
+			.catch(e => {
+				console.error(e)
+			})
+	}, [])
+
 	return (
 		<DailyImagesProvider>
 			<UploadProvider>
